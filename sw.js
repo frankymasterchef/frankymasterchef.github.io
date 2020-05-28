2df9ed57910cd260661a547e30ae3f157174c9c8
frankymasterchef <65267723+frankymasterchef@users.noreply.github.com>
2020-05-28 14:35:58 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 
-var CACHE_STATIC_NAME = 'static-v4';
-var CACHE_DYNAMIC_NAME = 'dynamic-v4';
+var CACHE_STATIC_NAME = 'static-v6';
+var CACHE_DYNAMIC_NAME = 'dynamic-v6';
 
 self.addEventListener('install', function(event) {
   console.log('[Service Worker] Installing Service Worker ...', event);
@@ -53,24 +53,26 @@ self.addEventListener('activate', function(event) {
 
 
 self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        if (response) {
-          return response;
-        } else {
-          return fetch(event.request)
-            .then(function(res) {
-              return caches.open(CACHE_DYNAMIC_NAME)
-                .then(function(cache) {
-                  cache.put(event.request.url, res.clone());
-                  return res;
+  if(event.request.url.indexOf('firestore.googleapis.com') === -1){
+      event.respondWith(
+        caches.match(event.request)
+          .then(function(response) {
+            if (response) {
+              return response;
+            } else {
+              return fetch(event.request)
+                .then(function(res) {
+                  return caches.open(CACHE_DYNAMIC_NAME)
+                    .then(function(cache) {
+                      cache.put(event.request.url, res.clone());
+                      return res;
+                    })
                 })
-            })
-            .catch(function(err) {
+                .catch(function(err) {
 
-            });
-        }
-      })
-  );
+                });
+            }
+          })
+      );
+    }
 });
