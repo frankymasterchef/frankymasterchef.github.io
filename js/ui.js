3fd8fff3e15d25554e1c14aac2385622e834f85a
Edwardh <edwardhosea1313@gmail.com>
2020-06-08 20:36:04 +0700

    reindent
    reindent

--- js/ui.js
+++ js/ui.js
@@ -13,13 +13,13 @@ const removeRecipe = (id) => {
     console.log("delete:" + `${id}`);
     const recipe= document.querySelector(`.recipe[data-id=${id}]`);
     recipe.remove();
-}; 
+};
 
 
 //page detail myrecipe
 const recipes_detail= document.querySelector('.detail');
 const renderdetailrecipe = (data,id) =>{
-    
+
     const html_ = `
     <div class="ftco-blocks-cover-1">
     <div class="site-section-cover bg-image overlay"  style="background-image: url('https://firebasestorage.googleapis.com/v0/b/webchefpad-1e9bd.appspot.com/o/static%2Fexplore_cover.webp?alt=media&token=5f76d7d5-0ad3-413e-b7de-72f1bd4edcc9')">
@@ -32,25 +32,14 @@ const renderdetailrecipe = (data,id) =>{
       </div>
     </div>
   </div>
-
-
-
-
   <div class="site-section">
     <div class="container">
-
-  
-
       <div class="row">
          <div class="col-md-12 mb-4">
             <div class="row">
              <div class="col-md-8 mb-4 mt-md-5">
                 <div class="d-flex align-items-center" id="image_recipe_detail">
-
-                 
                    <a href=${data.image} data-fancybox='gal'><img src=${data.image} alt='Image' class='img-fluid'></a>
-
-                
                 </div>
               </div>
               <div class="col-md-4 mt-md-5 sidebar">
@@ -63,17 +52,13 @@ const renderdetailrecipe = (data,id) =>{
             </div>
         </div>
       </div>
-
       <div class="row">
         <div class="col-md-8 blog-content">
           <div class="tab-content" id="v-pills-tabContent">
               <div class="tab-pane fade show active" id="v-pills-Ingredients" role="tabpanel" aria-labelledby="v-pills-Ingredients-tab">
-                
                  ${data.ingredients}<br>
-                 
               </div>
               <div class="tab-pane fade" id="v-pills-Step" role="tabpanel" aria-labelledby="v-pills-Step-tab">
-
                ${data.steps}<br><br>
               </div>
               <div class="tab-pane fade" id="v-pills-Tips" role="tabpanel" aria-labelledby="v-pills-Tips-tab">
@@ -81,30 +66,17 @@ const renderdetailrecipe = (data,id) =>{
               </div>
          </div>
         </div>
-
-
         <div class="col-md-2 sidebar">
-            
-            
             <div class="sidebar-box" id="edited_button">
-
              <a class="btn btn-primary text-white py-3 px-5" role ="button" href='edit_myrecipe.html?id="${id}"'><i class="icon-edit" href='edit_myrecipe.html?id="${id}"'></i></a>
-
             </div>
-
-            
           </div>
-        
       </div>
     </div>
   </div>
-
     `
-
     recipes_detail.innerHTML += html_;
 };
-
-
 //page edit myrecipe
 const edited= document.querySelector('.edit');
 const rendereditedRecipe = (data, id) => {
@@ -113,11 +85,9 @@ const rendereditedRecipe = (data, id) => {
   const tips= data.tips.replace(/<br\s*[\/]?>/gi, "\n");
   const html2 =`
     <div class="site-section">
-
     <div class="text-center">
       <h2 class="heading-border-bottom font-weight-bold serif text-black mb-5 text-center">Edit Your Recipe</h2>
     </div>
-
     <div class="site-section bg-image overlay features_75651" style="background-image: url('https://firebasestorage.googleapis.com/v0/b/webchefpad-1e9bd.appspot.com/o/static%2Fcreate_bg2.webp?alt=media&token=e38ffc37-4274-43cd-8257-d04db1b34700')">
       <div class="container">
         <div class="row">
@@ -128,22 +98,18 @@ const rendereditedRecipe = (data, id) => {
               <a class="nav-link" id="v-pills-Tips-tab" data-toggle="pill" href="#v-pills-Tips" role="tab" aria-controls="v-pills-Tips" aria-selected="false">Tips</a>
             </div>
           </div>
-
           <div class="col-lg-8 mb-5 mt-md-5">
             <form class="edit-recipe">
-
               <div class="form-group row">
                 <div class="col-md-10">
                   <input type="text" class="form-control" placeholder="Title" id="title" value="${data.title}" required>
                 </div>
               </div>
-
               <div class="form-group row">
                 <div class="col-md-10">
                   <input type="text" class="form-control" placeholder="Short Description" id="short_desc" value="${data.short_desc}" required>
                 </div>
               </div>
-
               <div class="tab-content" id="v-pills-tabContent">
                 <div class="tab-pane fade show active" id="v-pills-Ingredients" role="tabpanel" aria-labelledby="v-pills-Ingredients-tab">
                   <div class="form-group row">
@@ -167,7 +133,6 @@ const rendereditedRecipe = (data, id) => {
                   </div>
                 </div>
               </div>
-
               <!-- Upload image input-->
               <div class="form-group row">
                 <div class="col-md-10 mb-4">
@@ -178,9 +143,7 @@ const rendereditedRecipe = (data, id) => {
                       <label for="upload" class="btn btn-light m-0 rounded-pill px-4"> <i class="fa fa-cloud-upload mr-2 text-muted"></i><small class="text-uppercase font-weight-bold text-muted">Choose file</small></label>
                     </div>
                   </div>
-
                   <div class="image-area mt-4 mb-4" id="pic_area"><img id="imageResult" src="${data.image}" alt="" class="img-fluid rounded shadow-sm mx-auto d-block"></div>
-
                   <div class="form-group row">
                     <div class="col-md-8 mr-auto ml-auto mb-4">
                       <input type="submit" class="btn btn-block btn-primary text-white py-3 px-5 upload-group" value="Submit">
@@ -188,8 +151,6 @@ const rendereditedRecipe = (data, id) => {
                   </div>
                 </div>
               </div>
-
-
             </form>
           </div>
         </div>
@@ -198,16 +159,11 @@ const rendereditedRecipe = (data, id) => {
 
     </div>
   </div>
-  
+
   `;
   edited.innerHTML += html2;
 
 
 
-  
+
 };
-
-
-
-
-
