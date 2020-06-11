20e6bc2abe3fd73de42c3e73cdaccc7648f6ad7e
frankymasterchef <65267723+frankymasterchef@users.noreply.github.com>
2020-06-11 13:43:24 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -60,10 +60,10 @@ self.addEventListener('fetch', function(event) {
                     })
                 })
                 .catch(function(err) {
-                  return caches.open(CACHE_STATIC_NAME)
-                  .then(function(cache) {
-                    return cache.match('/offline.html');
-                  });
+                  // return caches.open(CACHE_STATIC_NAME)
+                  // .then(function(cache) {
+                  //   return cache.match('/offline.html');
+                  // });
                 });
             }
           })
