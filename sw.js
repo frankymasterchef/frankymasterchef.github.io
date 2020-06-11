f8b059b35ab7d19a917cc5134cd3d8564f34c16a
Edwardh <edwardhosea1313@gmail.com>
2020-06-11 16:29:36 +0700

    try cache without 404

--- sw.js
+++ sw.js
@@ -60,10 +60,6 @@ self.addEventListener('fetch', function(event) {
                     })
                 })
                 .catch(function(err) {
-                  return caches.open(CACHE_STATIC_NAME)
-                  .then(function(cache) {
-                    return cache.match('/offline.html');
-                  });
                 });
             }
           })
